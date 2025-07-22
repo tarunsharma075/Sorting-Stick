5eac59553b777b920e77ae1fb3a095dede9117e2
Tarun <tarunkumarsharma38@gmail.com>
2025-07-22 21:28:20 +0530

    Update MainMenuUIController.cpp

--- Sorting-Stick/source/UI/MainMenu/MainMenuUIController.cpp
+++ Sorting-Stick/source/UI/MainMenu/MainMenuUIController.cpp
@@ -80,7 +80,7 @@ namespace UI
             selection_sort_button->registerCallbackFuntion(std::bind(&MainMenuUIController::selectionSortButtonCallback, this));
             merge_sort_button->registerCallbackFuntion(std::bind(&MainMenuUIController::mergeSortButtonCallback, this));
             quick_sort_button->registerCallbackFuntion(std::bind(&MainMenuUIController::quickSortButtonCallback, this));
-            //radix_sort_button->registerCallbackFuntion(std::bind(&MainMenuUIController::radixSortButtonCallback, this));
+            radix_sort_button->registerCallbackFuntion(std::bind(&MainMenuUIController::radixSortButtonCallback, this));
             quit_button->registerCallbackFuntion(std::bind(&MainMenuUIController::quitButtonCallback, this));
         }
 
@@ -123,6 +123,8 @@ namespace UI
         void MainMenuUIController::radixSortButtonCallback()
         {
             ServiceLocator::getInstance()->getSoundService()->playSound(SoundType::BUTTON_CLICK);
+            GameService::setGameState(GameState::GAMEPLAY);
+            ServiceLocator::getInstance()->getGameplayService()->sortElement(Gameplay::Collection::SortType::RADIX_SORT);
         }
 
         void MainMenuUIController::quitButtonCallback()
